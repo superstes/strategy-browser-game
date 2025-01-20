67b6dcbac6e465083b0d826928c1fe100c1fc35b
Rath Pascal <rath@oxl.at>
2025-01-20 04:05:24 +0100

    add demo setup

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -12,6 +12,8 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 PATH_BASE="$(pwd)/.."
+PATH_FE="${PATH_BASE}/src/frontend"
+PATH_BE="${PATH_BASE}/src/backend"
 
 echo '### Testing for nodeJS ###'
 node -v >/dev/null
@@ -20,7 +22,6 @@ echo '### Downloading dependencies ###'
 bash init-lib.sh
 
 echo '### Downloading node-modules ###'
-PATH_FE="${PATH_BASE}/src/frontend"
 cd "$PATH_FE"
 
 if ! [ -d "${PATH_FE}/node_modules/" ]
@@ -47,3 +48,12 @@ URL_NODE_MOD="https://${DEMO_DOMAIN}/node_modules"
 patch_js_imports "as THREE from 'three'" "as THREE from '${URL_NODE_MOD}/three/src/Three.js'"
 patch_js_imports "from 'three/examples" "from '${URL_NODE_MOD}/three/examples"
 patch_js_imports "from 'dat.gui'" "from '${URL_NODE_MOD}/dat.gui/src/dat/index.js'"
+
+echo '### Generating map ###'
+cd "$PATH_BASE"
+VENV_PATH='/tmp/.sup-game-venv'
+
+python3 -m virtualenv "$VENV_PATH" >/dev/null
+source "${VENV_PATH}/bin/activate"
+pip install -r "${PATH_BE}/map_generator/requirements.txt" >/dev/null
+bash scripts/map-gen.sh >/dev/null
